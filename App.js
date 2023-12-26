157a2e18bebc6f3d995ff9d35abc0774dd43f13f
Marwa <marwakhalid558@gmail.com>
2023-12-26 23:46:19 +0500

    h

--- App.js
+++ App.js
@@ -7,20 +7,22 @@ import Homepage from './screens/Homepage';
 import PersonalLoan from './screens/PersonalLoan';
 import HomeLoan from './screens/HomeLoan';
 import BusinessLoan from './screens/BusinessLoan';
-
+import './App.css';
 const Stack = createNativeStackNavigator();
 
 const App = () =>{
   return (
-    <NavigationContainer style={styles.container}>
-      <Stack.Navigator  screenOptions={{ headerShown: false }}>
-        <Stack.Screen name='Splash' component={Splash}/>
-        <Stack.Screen name='Homepage' component={Homepage}/>
-        <Stack.Screen name='PersonalLoan' component={PersonalLoan}/>
-        <Stack.Screen name='HomeLoan' component={HomeLoan}/>
-        <Stack.Screen name='BusinessLoan' component={BusinessLoan}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.main}>
+      <NavigationContainer style={styles.container}>
+        <Stack.Navigator  screenOptions={{ headerShown: false }}>
+          <Stack.Screen name='Splash' component={Splash}/>
+          <Stack.Screen name='Homepage' component={Homepage}/>
+          <Stack.Screen name='PersonalLoan' component={PersonalLoan}/>
+          <Stack.Screen name='HomeLoan' component={HomeLoan}/>
+          <Stack.Screen name='BusinessLoan' component={BusinessLoan}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
 
@@ -30,6 +32,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin:0,
+    top:0
   },
 });
 
